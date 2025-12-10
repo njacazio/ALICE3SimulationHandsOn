94dce0d849e2e5bbe5ad338d57c443b3261098c7
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-10 12:35:57 +0100

    Update

--- Sessions/1_ACTS/conversion_to_ao2d/convert_to_ao2d.sh
+++ Sessions/1_ACTS/conversion_to_ao2d/convert_to_ao2d.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+InputPath="../full_chain_simple/acts_simulation_output/sim_pythia_pp/"
 
-InputPath="/home/njacazio/cernbox/Share/MKUltra/ACTS/odd_output/"
-InputPath="/home/njacazio/cernbox/Dropshipping/DATA/"
-
+rm AO2D_converted.root
+ln -s $PWD/.AO2D_1.root ${InputPath}/AO2D_1.root # Create a symlink to the utility AO2D file
 
 o2-analysis-alice3-tracking-translator --aod-file ${InputPath}/AO2D_1.root -b --aod-writer-keep dangling
+
+# unlink the utility AO2D file
+unlink ${InputPath}/AO2D_1.root
+
+mv AnalysisResults_trees.root AO2D_converted.root
