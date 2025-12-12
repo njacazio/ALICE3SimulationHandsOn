92c06a9b34d2f675edbaf71dd4a381e4baf4ff03
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-12 10:30:19 +0100

    Fix

--- Sessions/3_FastSimulation/analyse_tracked_events.sh
+++ Sessions/3_FastSimulation/analyse_tracked_events.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 o2-analysis-onthefly-tracker -b --configuration json://configuration.json |
-    o2-analysis-track-propagation -b --configuration json://configuration.json --track-propagation-output-filename propagated_tracks.root
+    o2-analysis-track-propagation -b --configuration json://configuration.json --aod-file AO2D_pp.root
