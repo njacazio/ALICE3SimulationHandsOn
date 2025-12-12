3d787a3382161294cb09eddc7c431895ecad6ec5
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-12 10:14:15 +0100

    Update

--- Sessions/3_FastSimulation/generate_events.sh
+++ Sessions/3_FastSimulation/generate_events.sh
@@ -2,14 +2,15 @@
 
 # This script generates the events and saves them to a file.
 
+# --configFile generator_configuration/pythia8_pp.ini -b \
 o2-sim-dpl-eventgen \
     --generator external \
-    --nEvents 100000 \
+    --nEvents 100 \
     --aggregate-timeframe 50 \
-    --configFile generator_configuration/pythia8_pp.ini -b \
+    --configFile generator_configuration/pythia8_PbPb.ini -b \
     --configuration json://configuration.json |
     o2-sim-mctracks-to-aod \
         --tf-offset 391403000000 -b \
         --configuration json://configuration.json \
         --aod-writer-keep dangling
-mv AnalysisResults_trees.root AO2D_pp.root
+mv AnalysisResults_trees.root AO2D_PbPb.root
--- Sessions/3_FastSimulation/track_generated_events.sh
+++ Sessions/3_FastSimulation/track_generated_events.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-o2-analysis-onthefly-tracker -b --configuration json://configuration.json --aod-writer-keep dangling
+o2-analysis-onthefly-tracker -b --configuration json://configuration.json --aod-writer-keep dangling --aod-file AO2D_pp.root
