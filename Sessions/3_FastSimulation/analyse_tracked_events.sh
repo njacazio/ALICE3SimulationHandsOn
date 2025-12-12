a2695bebfdd0226e1ed9c11a88fa5ee5ace5d82f
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-12 10:31:07 +0100

    U

--- Sessions/3_FastSimulation/analyse_tracked_events.sh
+++ Sessions/3_FastSimulation/analyse_tracked_events.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 o2-analysis-onthefly-tracker -b --configuration json://configuration.json |
-    o2-analysis-track-propagation -b --configuration json://configuration.json --aod-file AO2D_pp.root
+    o2-analysis-alice3-tracking-performance -b --configuration json://configuration.json --aod-file AO2D_pp.root
